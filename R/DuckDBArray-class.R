943ad09913b735af2cd4238ea5c21f175da1d1fe
Patrick Aboyoun <aboyoun.patrick@gene.com>
2024-12-05 14:40:40 -0800

    doc: Flesh out DuckDBArray-class.Rd

--- R/DuckDBArray-class.R
+++ R/DuckDBArray-class.R
@@ -1,4 +1,4 @@
-#' DuckDB tables as DelayedArray objects
+#' DuckDBArray objects
 #'
 #' @description
 #' The DuckDBArray class is a \link[DelayedArray]{DelayedArray} subclass
@@ -7,7 +7,75 @@
 #' All the operations available for \link[DelayedArray]{DelayedArray}
 #' objects work on DuckDBArray objects.
 #'
-#' @inheritParams DuckDBArraySeed-class
+#' @section Constructor:
+#' \describe{
+#'   \item{\code{DuckDBArray(conn, datacols, keycols, type = NULL)}:}{
+#'     Creates a DuckDBDataFrame object.
+#'     \describe{
+#'       \item{\code{conn}}{
+#'         Either a character vector containing the paths to parquet, csv, or
+#'         gzipped csv data files; a string that defines a duckdb \code{read_*}
+#'         data source; a DuckDBDataFrame object; or a tbl_duckdb_connection
+#'         object.
+#'       }
+#'       \item{\code{datacols}}{
+#'         Either a string specifying the column from \code{conn} or a named
+#'         \code{expression} that will be evaluated in the context of
+#'         \code{conn} that defines the values in the array.
+#'       }
+#'       \item{\code{keycols}}{
+#'         Either a character vector of column names from \code{conn} that will
+#'         specify the dimension names, or a named list of character vectors
+#'         where the names of the list specify the dimension names and the
+#'         character vectors set the distinct values for the dimension names.
+#'       }
+#'       \item{\code{type}}{
+#'         String specifying the type of the data values; one of
+#'         \code{"logical"}, \code{"integer"}, \code{"integer64"},
+#'         \code{"double"}, or \code{"character"}. If \code{NULL}, it is
+#'         determined by inspecting the data.
+#'       }
+#'     }
+#'   }
+#' }
+#'
+#' @section Accessors:
+#' In the code snippets below, \code{x} is a DuckDBArray object:
+#' \describe{
+#'   \item{\code{dim(x)}:}{
+#'     An integer vector of the array dimensions.
+#'   }
+#'   \item{\code{dimnames(x)}:}{
+#'     List of array dimension names.
+#'   }
+#'   \item{\code{type(x)}, \code{type(x) <- value}:}{
+#'     Get or set the data type of the array elements; one of \code{"logical"},
+#'     \code{"integer"}, \code{"integer64"}, \code{"double"}, or
+#'     \code{"character"}.
+#'   }
+#' }
+#'
+#' @section Subsetting:
+#' In the code snippets below, \code{x} is a DuckDBArray object:
+#' \describe{
+#'   \item{\code{x[i, j, ..., drop = TRUE]}:}{
+#'     Returns a new DuckDBArray object. Empty dimensions are dropped if
+#'     \code{drop = TRUE}.
+#'   }
+#' }
+#'
+#' @section Transposition:
+#' In the code snippets below, \code{x} is a DuckDBArray object:
+#' \describe{
+#'   \item{\code{aperm(a, perm)}:}{
+#'     Returns a new DuckDBArray object with the dimensions permuted
+#'     according to the \code{perm} vector.
+#'   }
+#'   \item{\code{t(x)}:}{
+#'     For two-dimensional arrays, returns a new DuckDBArray object with the
+#'     dimensions transposed.
+#'   }
+#' }
 #'
 #' @author Patrick Aboyoun
 #'
@@ -45,6 +113,8 @@
 #' @include DuckDBArraySeed-class.R
 #' @include DuckDBArraySeed-utils.R
 #'
+#' @keywords classes methods
+#'
 #' @name DuckDBArray-class
 NULL
 
@@ -81,7 +151,6 @@ setReplaceMethod("type", "DuckDBArray", function(x, value) {
 
 #' @export
 #' @importFrom S4Vectors new2
-#' @rdname DuckDBArray-class
 DuckDBArray <- function(conn, datacols, keycols, type = NULL) {
     if (!is(conn, "DuckDBArraySeed")) {
         conn <- DuckDBArraySeed(conn, datacols = datacols, keycols = keycols, type = type)
